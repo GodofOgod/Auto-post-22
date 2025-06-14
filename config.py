3969a205b2f09f8cf844d869c41e8538fefbbfb3
Krshna <infinityinfo0077@gmail.com>
2025-06-15 00:20:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ BOT_TOKEN = ""
 AUTHORIZED_USERS = [12345678]  
 DB_URL = "mongodb.srv......"
 DB_NAME = "NxMirror"
-DEFAULT_CHANNELS = [-100xxxx, -100xxxxx]  #Optional you can set unlimited channels from bot
+DEFAULT_CHANNELS = [-100xxxx, -100xxxxx] 
+#DEFAULT_CHANNELS variable is optional you can set unlimited channels from bot
