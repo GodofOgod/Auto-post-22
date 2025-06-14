b3a9120ce9c1d2489e3bc7b543f3cf0e76d557a6
Krshna <infinityinfo0077@gmail.com>
2025-06-15 00:23:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
-BOT_TOKEN = ""  
-AUTHORIZED_USERS = [12345678]  
-DB_URL = "mongodb.srv......"
+# Telegram Bot Token (from @BotFather)
+BOT_TOKEN = "your_bot_token_here"
+
+# List of Telegram user IDs who are authorized to use the bot
+AUTHORIZED_USERS = [12345678, 87654321]  # You can add more user IDs
+
+# MongoDB connection string and database name
+DB_URL = "mongodb+srv://username:password@cluster.mongodb.net"
 DB_NAME = "NxMirror"
-DEFAULT_CHANNELS = [-100xxxx, -100xxxxx] 
-#DEFAULT_CHANNELS variable is optional you can set unlimited channels from bot
+
+# Optional: List of default Telegram channels)
+# You can add unlimited channel directy from bot)
+DEFAULT_CHANNELS = [-1001234567890, -1009876543210]
