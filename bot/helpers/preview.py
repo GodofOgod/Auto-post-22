7a17c62518c74534361525ccb72071a61f736b3a
Krshna <infinityinfo0077@gmail.com>
2025-06-15 00:53:02 +0530

    Update preview.py

--- bot/helpers/preview.py
+++ bot/helpers/preview.py
@@ -1,3 +1,7 @@
+# © 2025 FtKrishna. All rights reserved.
+# Channel  : https://t.me/NxMirror
+# Contact  : @FTKrshna
+
 from bot.logger import setup_logger
 from aiogram import Bot
 from aiogram.types import InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, InputMediaDocument
