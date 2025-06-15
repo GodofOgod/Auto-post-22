3d06d2ae0dbd1ae4b12a513275ebd63bd56dd92e
Krshna <infinityinfo0077@gmail.com>
2025-06-15 07:35:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
-# Telegram Bot Token (from @BotFather)
-BOT_TOKEN = "your_bot_token_here"
-
-# List of Telegram user IDs who are authorized to use the bot
-AUTHORIZED_USERS = [12345678, 87654321]  # You can add more user IDs
-
-# MongoDB connection string and database name
-DB_URL = "mongodb+srv://username:password@cluster.mongodb.net"
-DB_NAME = "NxMirror"
-
-# Optional: List of default Telegram channels)
-# You can add unlimited channel directy from bot)
-DEFAULT_CHANNELS = [-1001234567890, -1009876543210]
-
 # © 2025 FtKrishna. All rights reserved.
 # Channel  : https://t.me/NxMirror
 # Contact  : @FTKrshna
+
+import os
+
+# Telegram Bot Token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "default_bot_token_if_missing")
+
+# List of authorized user IDs
+AUTHORIZED_USERS = list(map(int, os.environ.get("AUTHORIZED_USERS", "12345678,87654321").split(",")))
+
+# MongoDB URL and DB name
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://username:password@cluster.mongodb.net")
+DB_NAME = os.environ.get("DB_NAME", "NxMirror")
+
+# Optional: List of default Telegram channels)
+# You can add unlimited channel directy from bot)
+DEFAULT_CHANNELS = list(map(int, os.environ.get("DEFAULT_CHANNELS", "-1001234567890,-1009876543210").split(",")))
